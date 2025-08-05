be8c889063f3307ecf8e7796cf5335e29791fd35
byeongsu364 <kbsyjysbs@gmail.com>
2025-08-05 12:32:37 +0900

    update

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ const express = require("express")
 const app = express()
 const PORT = 3000
 
-const userRouter = require("./routes/user")
+const bookRoutes = require("./routes/books")
 
-app.use(express.json())
-app.use("/users", userRouter)
+app.use(express.json());
+app.use("/books", bookRoutes)
 
 
 
